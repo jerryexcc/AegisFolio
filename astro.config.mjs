5d717aafa996faf847964d1cc65d6454f4128b14
Killua <harima79215121@gmail.com>
2026-09-10 20:11:46 +0800

    fix display not show online

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()]
+  site: 'https://jerryexcc.github.io',
+  base: '/AegisFolio',
+  integrations: [vue()],
 });
